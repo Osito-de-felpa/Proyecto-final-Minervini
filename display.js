aecf0d3ff7a5a52cee9484d4a49f578e2f863c96
Osito-de-felpa <145238852+Osito-de-felpa@users.noreply.github.com>
2024-01-03 19:58:16 -0300

    Update display.js

--- display.js
+++ display.js
@@ -1,15 +1,14 @@
 function updateAccountList() {
     var accountList = document.getElementById('accountList');
-    accountList.innerHTML = '';
     
     // Retrieving the array from local storage
     var retrievedDataString = localStorage.getItem('data');
 
     // Parse the JSON string back to an array
-    var dota2 = JSON.parse(retrievedDataString);
+    var arr = JSON.parse(retrievedDataString);
 
     // Loop through the array and add rows to the table
-    dota2.forEach(function (account) {
+    arr.forEach(function (account) {
         var row = document.createElement('tr');
         row.innerHTML = `
             <td>${account.nombreUsuario}</td>
@@ -18,7 +17,24 @@ function updateAccountList() {
             <td>${account.contraseña}</td>
         `;
         accountList.appendChild(row);
+
+    // Convert the array to a JSON string
+    var dataString = JSON.stringify(arr);
+          
+    // Save the array in local storage
+    localStorage.setItem('data', dataString);
     });
+    
+}
+
+function deleteElementByIndex() {
+    var indexToDelete = parseInt(indiceEliminar.value)
+    if (indexToDelete < 0 || indexToDelete >= myArray.length) {
+        Alert('indice inexistente, por favor ingrese un indice existente.');
+        return
+    }
+    data.splice(indexToDelete = parseInt(indiceEliminar.value), 1);
+    return data;
 }
 
 
